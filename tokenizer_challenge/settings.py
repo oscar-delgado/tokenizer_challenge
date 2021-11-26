2168128bc59ce29a0d4baacfb587eb4c136e742c
oscar-delgado <odelgado@flumotion.com>
2021-11-26 17:10:54 +0100

    Deleted database

--- tokenizer_challenge/settings.py
+++ tokenizer_challenge/settings.py
@@ -74,10 +74,7 @@ WSGI_APPLICATION = 'tokenizer_challenge.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {}
 }
 
 
